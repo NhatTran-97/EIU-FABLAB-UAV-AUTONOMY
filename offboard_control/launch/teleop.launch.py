f52aff1b648e8705e071b0c76e4de59bd31144ff
NhatTran-97 <nhat.tranduy1997@gmail.com>
2026-04-18 18:40:04 +0700

    Added skydroid packages

--- offboard_control/launch/teleop.launch.py
+++ offboard_control/launch/teleop.launch.py
@@ -3,19 +3,20 @@ from launch_ros.actions import Node
 import os
 from ament_index_python.packages import get_package_share_directory
 
+
 def generate_launch_description():
 
-    offboard_pkg = get_package_share_directory("offboard_control")
+    config = get_package_share_directory("offboard_control")
 
-    offboard_node = Node(
+    teleop_node = Node(
             package='offboard_control',
-            executable='offboard_mode',
-            name='offboard_node',
+            executable='teleop_executor',
+            name='teleop_executor',
             output='screen',
-            parameters=[os.path.join(offboard_pkg, "config", "params.yaml")],
+            parameters=[os.path.join(config, "config", "params.yaml")],
         )
 
     return LaunchDescription([
-        offboard_node
+        teleop_node
 
     ])
